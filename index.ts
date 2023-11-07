8d18b4ca09dde156dfe4b014a3cd39ecde1aa795
0x0Koda <12773720+0x0Koda@users.noreply.github.com>
2023-11-07 15:59:52 +1100

    first commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+import axios, { AxiosRequestConfig } from "axios";
+dotenv.config();
+
+async function sendCalldataTransaction() {
+  if (!process.env.PK) throw new Error("process.env.PK is undefined");
+  const PK = process.env.PK;
+
+  let squidCalldata;
+  try {
+    const url = "https://testnet.v2.api.squidrouter.com/v2/route";
+    const data = {
+      fromChain: "5",
+      fromToken: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+      fromAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
+      fromAmount: "1000000",
+      toChain: "grand-1",
+      toToken: "uusdc",
+      toAddress: "noble1zqnudqmjrgh9m3ec9yztkrn4ttx7ys64p87kkx",
+      quoteOnly: false,
+      slippage: 1,
+      slippageConfig: {
+        autoMode: 1,
+      },
+      enableBoost: false,
+    };
+
+    const headers: AxiosRequestConfig = {
+      headers: {
+        "x-integrator-id": "Squid-team", // Set your authorization token here.
+      },
+    };
+
+    const response = await axios.post(url, data, headers);
+    console.log("Response data:", response.data);
+    squidCalldata = response.data.route.transactionRequest.data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  const rpcUrl = "https://rpc.ankr.com/eth_goerli";
+  const provider = ethers.getDefaultProvider(rpcUrl);
+  const wallet = new ethers.Wallet(PK, provider);
+
+  // CCTP contract
+  const contractAddress = "0xD0C3da58f55358142b8d3e06C1C30c5C6114EFE8";
+
+  // Create a transaction object
+  const transaction = {
+    to: contractAddress,
+    data: squidCalldata,
+  };
+
+  // Estimate gas cost
+  const gasEstimate = await wallet.estimateGas(transaction);
+  transaction["gasLimit"] = gasEstimate;
+
+  // Sign and send the transaction
+  const tx = await wallet.sendTransaction(transaction);
+  console.log("Transaction sent:", tx.hash);
+
+  // Wait for the transaction to be confirmed
+  await tx.wait();
+  console.log("Transaction confirmed in block:", tx);
+}
+
+sendCalldataTransaction()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Error:", error);
+    process.exit(1);
+  });
